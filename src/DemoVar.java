396c7a133643d599a984777046caa93d504f6d39
up1 <somkiat.p@gmail.com>
2019-07-15 15:21:04 +0700

    Using var

---
+++ src/DemoVar.java
@@ -0,0 +1,17 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoVar {
+
+    public static void main(String[] args) {
+
+        List<Integer> oldInput = new ArrayList<>();
+        oldInput.add(1);
+
+        var newInput = new ArrayList<Integer>();
+        newInput.add(1);
+
+    }
+
+}
